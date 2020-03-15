5d8f3e5a555b488b0b3ca57e95a01ac6acd70323
DESKTOP-0RPC9CL\pawan <pawankumar219877@gmail.com>
2020-03-15 14:18:14 +0530

    shared module  is being called on other module component

--- src/app/feature/category/category.module.ts
+++ src/app/feature/category/category.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { AddCategoryComponent } from './add-category/add-category.component';
 
 import { CategoryRoutingModule }  from './category-routing.module'
-
+import { SharedModule }  from '../../shared/shared.module'
 @NgModule({
   declarations: [AddCategoryComponent],
   imports: [
     CommonModule,
+    SharedModule.forRoot(),
     CategoryRoutingModule
   ]
 })
---
+++ src/app/shared/components/required-field/required-field.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-required-field',
+  templateUrl: './required-field.component.html',
+  styleUrls: ['./required-field.component.css']
+})
+export class RequiredFieldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
